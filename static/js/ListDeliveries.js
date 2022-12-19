e96f4e3357bf330b781d1eddaac33815955c9f8d
andreaDiClaudio <andreadicla@gmail.com>
2022-12-19 16:56:32 +0100

    Checkpoint in creating filter option for delivery table

--- static/js/ListDeliveries.js
+++ static/js/ListDeliveries.js
@@ -3,19 +3,81 @@ console.log("Inside LisDeliveries")
 const getDeliveriesUrl = "http://localhost:8080/deliveries"
 
 //Elements from Html
-const deliveryTableBody =  document.getElementById("delivery-table-body")
+const deliveryTableBody = document.getElementById("delivery-table-body")
+const deliveryDropDown = document.querySelector("#delivery-drop-down")
+
+let warehouseArray = []
 
 //onload
-window.onload = loadDeliveryTable()
+window.onload = loadDeliveryFunctions()
+
+function loadDeliveryFunctions() {
+    loadDeliveryTable()
+    fillDropDownDelivery()
+}
+
+
+//Function that redirects the creation of the table based on the value selected in the dropdown
+function checkDropDownValue() {
+    if (document.querySelector("#delivery-drop-down").value == "all") {
+        loadDeliveryTable()
+    } else {
+        loadTableById()
+    }
+}
 
 //Function to fetch Products
 async function getDeliveries() {
     return (await fetch(getDeliveriesUrl)).json()
 }
 
+//Function to fill dropdown Product
+async function fillDropDownDelivery() {
+
+    const options = await getDeliveries()
+
+    let opt = document.createElement('option');
+    opt.id = "all";
+    opt.value = "all"
+    opt.innerHTML = "All";
+    deliveryDropDown.appendChild(opt);
+
+
+    //Filters only unique warehouses
+    options.forEach(delivery => {
+        if (warehouseArray.includes(delivery.fromWarehouse)) {
+
+        } else {
+            warehouseArray.push(delivery.fromWarehouse)
+        }
+    })
+
+    console.log(warehouseArray)
+
+    for (let i = 0; i < warehouseArray.length; i++) {
+
+        let opt = document.createElement('option');
+
+        opt.id = warehouseArray[i];
+        opt.value = warehouseArray[i];
+        opt.innerHTML = warehouseArray[i];
+
+        deliveryDropDown.appendChild(opt);
+    }
+}
+
+
+//TASK 2 - Find a Specific Product - DONE
+async function loadTableByWarehouse() {
+    const product = await getDeliveryByWarehouse(document.querySelector("#drop-down").value)
+    deliveryTableBody.innerHTML = "";
+    createProductTable(product)
+}
+
 //Function to load the table
 async function loadDeliveryTable() {
     let productsArray = await getDeliveries()
+    deliveryTableBody.innerHTML = "";
     productsArray.forEach(delivery => createDeliveryTable(delivery))
 }
 
